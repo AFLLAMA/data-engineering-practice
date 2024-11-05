6dc2e3974ee4215c38d5732a57ee487a9ccbaaac
afllama <andriiflama@gmail.com>
2024-11-05 23:31:59 +0100

    task_group

--- airflow/dags/sales_local_gcs.py
+++ airflow/dags/sales_local_gcs.py
@@ -6,34 +6,33 @@ from airflow.providers.google.cloud.transfers.local_to_gcs import (
 )
 from datetime import datetime
 from airflow.models import Variable
-from airflow.models.param import Param
+from airflow.decorators import task_group
 
 
 with DAG(
     "save_json_to_gcs",
-    start_date=datetime(2024, 10, 31),
+    start_date=datetime(2022, 8, 9),
+    end_date=datetime(2022, 8, 11),
     schedule_interval=None,
     catchup=False,
     tags=["R_D"],
 ) as dag:
-    start = EmptyOperator(task_id="start")
-    end = EmptyOperator(task_id="end")
-    date = Variable.get("JSON_TO_GCS_DATE")
-    folder_path = os.path.join("/mnt", "data", "raw", "sales", date)
+    @task_group()
+    def group1():
+        date = "{ ds }"
+        folder_path = os.path.join("/mnt", "data", "raw", "sales", date)
 
-    if os.path.exists(folder_path):
-        for filename in os.listdir(folder_path):
-            file_path = os.path.join(folder_path, filename)
+        if os.path.exists(folder_path):
+            for filename in os.listdir(folder_path):
+                file_path = os.path.join(folder_path, filename)
 
-            upload_task = LocalFilesystemToGCSOperator(
-                task_id=f"upload_{filename}_to_gcs",
-                src=file_path,
-                dst=f"{'/'.join(date.split('-'))}/{filename}",
-                bucket=Variable.get("BUCKET_NAME"),
-                gcp_conn_id="my_gcs_connection_id",
-            )
-
-            # Define tasks for each local file in selected date
-            start >> upload_task >> end
-    else:
-        raise FileNotFoundError(f"The folder {folder_path} does not exist.")
+                upload_task = LocalFilesystemToGCSOperator(
+                    task_id=f"upload_{filename}_to_gcs",
+                    src=file_path,
+                    dst=f"{'/'.join(date.split('-'))}/{filename}",
+                    bucket=Variable.get("BUCKET_NAME"),
+                    gcp_conn_id="my_gcs_connection_id",
+                )                
+        else:
+            raise FileNotFoundError(f"The folder {folder_path} does not exist.")
+    group1()
